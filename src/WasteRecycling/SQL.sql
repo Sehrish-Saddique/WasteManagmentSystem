ce68f576cbf64933578bb99bc0a5a0d732f2579e
sehr <sehr.sehr1112@gmail.com>
2023-12-02 17:16:49 +0500

    Added sql file

---
+++ src/WasteRecycling/SQL.sql
@@ -0,0 +1,11 @@
+create database swing_demo
+CREATE TABLE student
+( id int NOT NULL,
+  name varchar(250) NOT NULL,
+  password varchar(250)
+);
+
+INSERT INTO student (id, name, password)
+VALUES (1, 'Ramesh', 'Ramesh@123');
+SELECT * From student 
+Go
