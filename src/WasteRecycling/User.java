62a9e757669d5ff2b96aecb7a70cbd0eca500191
sehr <sehr.sehr1112@gmail.com>
2023-12-02 15:32:49 +0500

    Generate Waste App and add User class

---
+++ src/WasteRecycling/User.java
@@ -0,0 +1,29 @@
+package WasteRecycling;
+
+public class User {
+	String name;
+	String age;
+	String id;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+	
+}
---
+++ src/WasteRecycling/main.java
@@ -0,0 +1,14 @@
+package WasteRecycling;
+import WasteRecycling.User;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Welcome to waste recyling system");
+		User obj = new User();
+		obj.setAge("34");
+		System.out.println(obj.getAge());
+		
+	}
+
+}
